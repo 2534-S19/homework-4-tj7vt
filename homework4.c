39396b0c481c7583edb6b5e97077205f159ba0fe
Thomas Koonce Jr <Thomas Koonce Jr@172.16.0.5>
2020-04-09 19:42:48 -0400

    Finished assignment

--- homework4.c
+++ homework4.c
@@ -4,42 +4,77 @@
 int main(void)
 {
     char rChar;
-    char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
+    char response[] = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
+    bool charAvail, entry = false;
+    static stateMachine_t print = first;
+    int i = 0;
 
 
     // Stops the Watchdog timer.
     initBoard();
+
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
+    eUSCI_UART_Config uartConfig = {
+         EUSCI_A_UART_CLOCKSOURCE_SMCLK,               // SMCLK Clock Source = 3MHz
+         19,                                           // UCBR   = 312 //divided by 16
+         8,                                            // UCBRF  = 8 //modulo 16
+         0x55,                                         // UCBRS  = 0x55 //modulation pattern decided by the fraction part
+         EUSCI_A_UART_NO_PARITY,                       // No Parity
+         EUSCI_A_UART_LSB_FIRST,                       // LSB First
+         EUSCI_A_UART_ONE_STOP_BIT,                    // One stop bit
+         EUSCI_A_UART_MODE,                            // UART mode
+         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION // Oversampling
+    };
 
 
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
-
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     // TODO: Initialize EUSCI_A0
-
+    UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
     // TODO: Enable EUSCI_A0
+    UART_enableModule(EUSCI_A0_BASE);
 
 
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
-
+        if (UART_getInterruptStatus (EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG) == EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG) {
+            rChar = UART_receiveData(EUSCI_A0_BASE);
+            charAvail = true;
+        }
+        else {
+            rChar = 0xFF;
+            charAvail = false;
+        }
 
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
         //       Check the transmit interrupt flag prior to transmitting the character.
-
+        if (UART_getInterruptStatus (EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG && charAvail == true) {
+            UART_transmitData(EUSCI_A0_BASE, rChar);
+            entry = charFSM(rChar, &print);
+        }
 
 
         // TODO: If the FSM indicates a successful string entry, transmit the response string.
         //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
         //       Make sure to reset the success variable after transmission.
+        if (entry == true) {
+            while (response[i] != '\0') {
+                if (UART_getInterruptStatus (EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) {
+                    UART_transmitData(EUSCI_A0_BASE, response[i]);
+                    i++;
+                }
+            }
 
-
+            i = 0;
+            entry = false;
+        }
     }
 }
 
@@ -49,10 +84,30 @@ void initBoard()
 }
 
 // TODO: FSM for detecting character sequence.
-bool charFSM(char rChar)
+bool charFSM(char rChar, stateMachine_t *print)
 {
     bool finished = false;
 
+    if (rChar == '2') {
+        *print = second;
+        finished = false;
+    }
+    else if (rChar == '5' && *print == second) {
+        *print = third;
+        finished = false;
+    }
+    else if (rChar == '3' && *print == third) {
+        *print = fourth;
+        finished = false;
+    }
+    else if (rChar == '4' && *print == fourth) {
+        finished = true;
+        *print = first;
+    }
+    else {
+        *print = first;
+        finished = false;
+    }
 
     return finished;
 }
--- homework4.h
+++ homework4.h
@@ -1,18 +1,19 @@
 /*
  * homework4.h
  *
- *  Created on:
- *      Author:
+ *  Created on: 04/05/2020
+ *      Author: TJ Koonce
  */
 
 #ifndef HOMEWORK4_H_
 #define HOMEWORK4_H_
 
+typedef enum {first, second, third, fourth} stateMachine_t;
+
 // This function initializes the board by turning off the Watchdog Timer.
 void initBoard();
-bool charFSM(char rChar);
+bool charFSM(char rChar, stateMachine_t *print);
 
 // TODO: Define any constants that are local to homework.c using #define
-
 
 #endif /* HOMEWORK4_H_ */
